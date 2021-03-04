6925b8e0243ed072adedabb23e831405d11dfef2
Shmoo <shmooritchie@gmail.com>
2021-03-04 10:11:59 -0800

    Rename some funcs.

--- src/UniqueItem.js
+++ src/UniqueItem.js
@@ -7,17 +7,17 @@ export default class UniqueItem extends Component {
     this.state = {
       openItem: false
     }
-    this.handleClick = this.handleClick.bind(this)
+    this.handleOpen = this.handleOpen.bind(this)
     this.handleClose = this.handleClose.bind(this)
-    this.handleSelect = this.handleSelect.bind(this)
+    this.handleOptionSelect = this.handleOptionSelect.bind(this)
   }
-  handleSelect(option) {
+  handleOptionSelect(option) {
     this.props.onSelect(option)
     if (option === 'option1') {
       this.handleClose()
     }
   }
-  handleClick() {
+  handleOpen() {
     this.setState({openItem: true})
   }
   handleClose() {
@@ -28,11 +28,11 @@ export default class UniqueItem extends Component {
     let uniqueItem = validOption ? this.props.item.item : null
     let content = validOption ? this.props.item.content : null
     let options = validOption && this.props.item.options ? Object.keys(this.props.item.options).map((key, i) => {
-      return <button key={i} onClick={() => this.handleSelect(key)}>{this.props.item.options[key].option}</button>
+      return <button key={i} onClick={() => this.handleOptionSelect(key)}>{this.props.item.options[key].option}</button>
     }) : null
     return(
       <div>
-        <div onClick={this.handleClick}>{uniqueItem}</div>
+        <div onClick={this.handleOpen}>{uniqueItem}</div>
         {this.state.openItem ? <UniqueItemContent content={content} options={options} onClick={this.handleClose} /> : null }
       </div>
     )

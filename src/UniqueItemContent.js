c6d4a604675fc7fd267f27e3d2bdad98dda89e65
Shmoo <shmooritchie@gmail.com>
2021-03-04 17:43:34 -0800

    Put input form on blank walls and print input to console.

--- src/UniqueItemContent.js
+++ src/UniqueItemContent.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 export default function UniqueItemContent(props) {
   const uniqueItemContentStyle = {
     position: 'absolute',
@@ -10,7 +12,14 @@ export default function UniqueItemContent(props) {
     alignItems: 'center',
     background: 'white'
   }
-  
+  const [ input, setInput ] = useState('')
+  function handleChange(event) {
+    setInput(event.target.value)
+  }
+  function handleSubmit(event) {
+    console.log(input)
+    event.preventDefault()
+  }
   const options = props.options ? 
     Object.keys(props.options).map((option, i) => {
       if (props.options[option].option) {
@@ -19,9 +28,16 @@ export default function UniqueItemContent(props) {
         return false 
       }
     }) : null
+
+  const content = props.content ? props.content : (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={input} onChange={handleChange} />
+      <input type="submit" value="Enter" />
+    </form>
+  )
   return (
     <div style={uniqueItemContentStyle}>
-      <div>{props.content}</div>
+      <div>{content}</div>
       <div>
         {options}
       </div>

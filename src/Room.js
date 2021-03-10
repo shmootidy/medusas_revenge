3da299ae7a82a5fd74d25f8c6b67cc82337badfb
Shmoo <shmooritchie@gmail.com>
2021-03-10 13:12:05 -0800

    Unhide player.

--- src/Room.js
+++ src/Room.js
@@ -139,7 +139,7 @@ export default class Room extends Component {
               <FloorItem item={floorItems.right.item} prize={floorItems.right.prize} position='right' onPickUp={this.handleFloorItems} />
               <UniqueItem room={this.state.currentRoom} position="right-door" item={this.state.rooms[this.state.currentRoom].uniqueItems} onSelect={this.selectItemOption} inventory={this.props.inventory} handleWriting={this.handleWriting} />
             </div>
-            {/* <Player /> */}
+            <Player />
             <div style={{width: '100%', display: 'flex', justifyContent: 'center'}}><Door door={doors.back} onDoorClick={this.handleDoorClick} /></div>
           </div>
         </div>

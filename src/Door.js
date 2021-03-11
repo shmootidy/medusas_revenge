2a95f7e6816c2485bb806286d245103206957bcd
Shmoo <shmooritchie@gmail.com>
2021-03-11 12:40:16 -0800

    Show doors from assets, and style a little in css. I'm going to have to rearrange the html structure.

--- src/Door.js
+++ src/Door.js
@@ -1,5 +1,9 @@
 import { Component } from 'react'
 import icons from './icon-map'
+import doors_data from './assets/spritesheet.json'
+import forward_doorway from './assets/forward_doorway.png'
+import left_doorway from './assets/left_doorway.png'
+import right_doorway from './assets/right_doorway.png'
 
 export default class Door extends Component {
   constructor(props) {
@@ -16,9 +20,20 @@ export default class Door extends Component {
       height: '50px',
       width: '50px',
     }
+    console.log(doors_data.frames[door.position + '_doorway.png'])
+    let _door
+    if (door.position === 'forward') {
+      _door = forward_doorway
+    } else if (door.position === 'left') {
+      _door = left_doorway
+    } else if (door.position === 'right') {
+      _door = right_doorway
+    }
+    const _className = door.position + '-door door'
     return (
-      <div style={doorStyle} onClick={this.handleClick}>
-        <div>{ status }</div>
+      <div className={_className} style={doorStyle} onClick={this.handleClick}>
+        {/* <div>{ status }</div> */}
+        <img src={_door} alt="door" />
       </div>
     )
   }
